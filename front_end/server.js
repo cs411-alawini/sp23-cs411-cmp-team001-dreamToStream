ac4c107585a1b20deb2b7b07f1e668f48c1a4821
Anthony Doan <=>
2023-04-11 09:35:12 -0500

    Fix update rating to use user ID and show output after

--- front_end/server.js
+++ front_end/server.js
@@ -1,26 +1,4 @@
-/*
-// THIS IS ALL OLD STUFF FROM THE TUTORIAL IN CLASS
-// Importing the packages and assigning them to variables. Similar to include
-var express = require('express');
-var bodyParser = require('body-parser');
-var mysql = require('mysql2');
-
-
 // Initializing the application
-var app = express();
-
-// Write properties to define how server behaves when user interacts with it
-// '/' is the base path
-app.get('/', function(req, res) {
-        res.send({'message': 'Hello'});
-});
-
-// Provide port number that the app will listen on
-app.listen(80, function () {
-    console.log('Node app is running on port 80');
-});
-*/
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql2');
@@ -130,59 +108,85 @@ app.get('/success', function(req, res) {
   res.render('success', { title: 'Success' });
 });
 
-app.get('/rating', function(req, res) {
-  res.render('rating', { title: 'Add a Rating' });
+app.get('/update-rating', function(req, res) {
+  var userId = req.query.id; ; // Get the user ID from the URL parameter
+  res.render('update-rating', { title: 'Update a Rating', userId: userId });
 });
 
 /* POST request to create user, redirect to success page if successful, show error message if unsuccessful */
-app.post('/rating', function(req, res) {
+app.post('/update-rating', function(req, res) {
+  var userId = req.query.id; // Retrieve user ID from URL parameter
   var media = req.body.media;
   var rating = req.body.rating;
   var type = req.body.type;
 
   if (type = 'Movie'){
-    var sql2 = `UPDATE MovieRating SET value = ${rating} WHERE name = '${media}'`;
+    var type = 'MovieRating';
+    var sql2 = `UPDATE MovieRating SET value = ${rating} WHERE id = ${userId} AND name = '${media}'`;
 
 
   }
   else {
-    var sql2 = `UPDATE ShowRating SET value = ${rating} WHERE name = '${media}'`;
+    var type = 'ShowRating';
+    var sql2 = `UPDATE ShowRating SET value = ${rating} WHERE id = ${userId} AND  name = '${media}'`;
 
   }
 
-    connection.query(sql2, function(err, result) {
-      if (err) {
-        res.send(err);
-        return;
-      }
-      console.log('Rating Updated');
-      res.redirect('/success');
-    });
+  connection.query(sql2, function(err, result) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    console.log('Rating Updated');
+    res.redirect(`/rating-success?type=${type}&id=${userId}`);
   });
- 
-  //GET request for shows
-  app.get('/shows', function(req, res) {
-    res.render('shows', { title: 'Shows' });
+});
+
+
+// GET request for rating success page
+app.get('/rating-success', function(req, res) {
+  // Determine the appropriate table to query based on the 'type' parameter
+  var tableName = req.query.type;
+  var userID = req.query.id;
+
+  // Perform a SELECT query to retrieve the updated data from the database
+  var sql = `SELECT * FROM ${tableName} WHERE id = ${userID}`;
+
+  connection.query(sql, function(err, results) {
+    if (err) {
+      res.send(err);
+      return;
+    }
+
+    // Render the rating success page with the retrieved data
+    res.render('rating-success', { ratings: results });
   });
-  
-  //GET request for search
-  app.get('/search', (req, res) => {
-      var title = req.query.title
-      var genre = req.query.genre;
-    
-      let sql = `SELECT * FROM Shows WHERE  (name like '${title}%' OR name like '${title}%') AND genre LIKE '%${genre}%' LIMIT 15`
-      // let sql = `SELECT * FROM Shows LIMIT 15`
-    
-        connection.query(sql, (err, result) => {
-          if (err) {
-            res.send(err);
-            return;
-          }
-          // res.render('shows', { title: 'Shows' });
-          // res.re(result)
-          res.send(result)
-        });
-      });
+});
+
+
+//GET request for shows
+app.get('/shows', function(req, res) {
+  res.render('shows', { title: 'Shows' });
+});
+
+//GET request for search
+app.get('/search', (req, res) => {
+  var title = req.query.title
+  var genre = req.query.genre;
+
+  let sql = `SELECT * FROM Shows WHERE  (name like '${title}%' OR name like '${title}%') AND genre LIKE '%${genre}%' LIMIT 15`
+  // let sql = `SELECT * FROM Shows LIMIT 15`
+
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    // res.render('shows', { title: 'Shows' });
+    // res.re(result)
+    res.send(result)
+  });
+});
 
 //Generated Data for Advanced Queries -- REPLACE with sql outputs
 var dataQuery1 = [
@@ -190,112 +194,27 @@ var dataQuery1 = [
   ["Movies ",'2021','432'],
   ["Shows",'2020','98'],
   ['Movies','2020','435']
-  ]; 
+]; 
 
 var dataQuery2 = [
   ["Divya",'Manirajan','23','11-02-1999'],
   ["John ",'Smith','43','1-04-1949'],
   ["boe",'jonnson','98','3-0-3333'],
   ['hadf','adsf','435','2-2-2220']
-  ]; 
+]; 
 
 
 //GET request to set up advanced queries
-  app.get('/AdvQueriesSubmit', function(req,res){
-    res.render('AdvQueriesSubmit');
-  });
+app.get('/AdvQueriesSubmit', function(req,res){
+  res.render('AdvQueriesSubmit');
+});
 
 //POST request for Advanced Query 1
-  app.post('/Query1', function(req, res) {
-    res.render('AdvQuery1Table', {sampleData:dataQuery1})}
-  );  
+app.post('/Query1', function(req, res) {
+  res.render('AdvQuery1Table', {sampleData:dataQuery1})}
+);  
 
-  //POST request for Advanced Query 2
+//POST request for Advanced Query 2
 app.post('/Query2', function(req, res) {
   res.render('AdvQuery2Table', {sampleData:dataQuery2})}
-  ); 
-
-
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var mysql = require('mysql2');
-// var path = require('path');
-// var connection = mysql.createConnection({
-//                 host: '35.226.99.157',
-//                 user: 'root',
-//                 password: 'dreamToStream',
-//                 database: 'dreamToStream'
-// });
-
-// connection.connect;
-
-
-// var app = express();
-
-// // set up ejs view engine 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
- 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(__dirname + '../public'));
-
-// /* GET home page, respond by rendering index.ejs */
-// app.get('/', function(req, res) {
-//   res.render('index', { title: 'Create User' });
-// });
-
-// app.get('/success', function(req, res) {
-//       res.send({'message': 'User created successfully!'});
-// });
- 
-// // this code is executed when a user clicks the form submit button
-// app.post('/mark', function(req, res) {
-//   var firstName = req.body.firstName;
-//   var lastName = req.body.lastName;
-//   var dob = req.body.dateOfBirth;
-//   var country = req.body.country;
-
-//   connection.query('SELECT MAX(id) AS max_id FROM Users', function(err, result) {
-//     if (err) {
-//       res.send(err);
-//       return;
-//     }
-
-//     // Get the next id to create a new user
-//     var max_id = result[0].max_id;
-//     var next_id = max_id ? max_id + 1 : 1;
-
-//     var sql = `INSERT INTO Users VALUES (${next_id}, '${firstName}', '${lastName}', '${dob}', '${country}')`;
-//     connection.query(sql, function(err, result) {
-//       if (err) {
-//         res.send(err);
-//         return;
-//       }
-//       console.log('New user inserted with id: ' + next_id);
-//       res.redirect('/success');
-//     });
-//   });
-// });
-
-// // create a new route to execute SELECT query
-// app.get('/shows', function(req, res) {
-//   var sql = 'SELECT * FROM Shows LIMIT 15';
-
-//   connection.query(sql, function(err, results, fields) {
-//     if (err) {
-//       console.error('Error executing MySQL query: ' + err.stack);
-//       return res.status(500).json({ error: 'Internal server error' });
-//     }
-
-//     // render shows.ejs file with query results
-//     res.render('shows', { shows: results });
-//   });
-// });
-
-
-
-// app.listen(80, function () {
-//     console.log('Node app is running on port 80');
-// });
+); 
