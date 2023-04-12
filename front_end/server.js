bda2c1d512fc80e27ce2d40475e607d9270f3e5e
rohanjs3 <rohanjs3@illinois.edu>
2023-04-11 21:26:19 -0500

    added delete

--- front_end/server.js
+++ front_end/server.js
@@ -17,29 +17,29 @@ var app = express();
 // set up ejs view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
- 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '../public'));
 
 /* GET home page, respond by rendering index.ejs */
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index', { title: 'Home' });
 });
 
 /* GET create user page, respond by rendering create-user.ejs */
-app.get('/create-user', function(req, res) {
+app.get('/create-user', function (req, res) {
   res.render('create-user', { title: 'Create User' });
 });
 
 /* POST request to create user, redirect to success page if successful, show error message if unsuccessful */
-app.post('/create-user', function(req, res) {
+app.post('/create-user', function (req, res) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var dob = req.body.dob;
   var country = req.body.country;
 
-  connection.query('SELECT MAX(id) AS max_id FROM Users', function(err, result) {
+  connection.query('SELECT MAX(id) AS max_id FROM Users', function (err, result) {
     if (err) {
       res.send(err);
       return;
@@ -50,7 +50,7 @@ app.post('/create-user', function(req, res) {
     var next_id = max_id ? max_id + 1 : 1;
 
     var sql = `INSERT INTO Users VALUES (${next_id}, '${firstName}', '${lastName}', '${dob}', '${country}')`;
-    connection.query(sql, function(err, result) {
+    connection.query(sql, function (err, result) {
       if (err) {
         res.send(err);
         return;
@@ -62,16 +62,16 @@ app.post('/create-user', function(req, res) {
 });
 
 // Create a new GET route for the sign-in page
-app.get('/sign-in', function(req, res) {
+app.get('/sign-in', function (req, res) {
   res.render('sign-in', { title: 'Sign In' });
 });
 
 // Handle sign-in form submissions
-app.post('/sign-in', function(req, res) {
+app.post('/sign-in', function (req, res) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
 
-  connection.query(`SELECT * FROM Users WHERE firstName='${firstName}' AND lastName='${lastName}'`, function(err, results, fields) {
+  connection.query(`SELECT * FROM Users WHERE firstName='${firstName}' AND lastName='${lastName}'`, function (err, results, fields) {
     if (err) {
       res.send(err);
       return;
@@ -86,10 +86,10 @@ app.post('/sign-in', function(req, res) {
 });
 
 // Handle sign in to homepage for users based on their given id
-app.get('/home/:id', function(req, res) {
+app.get('/home/:id', function (req, res) {
   var userId = req.params.id;
   // Fetch user data based on the userId from the database
-  connection.query('SELECT * FROM Users WHERE id = ?', [userId], function(err, results) {
+  connection.query('SELECT * FROM Users WHERE id = ?', [userId], function (err, results) {
     if (err) {
       res.send(err);
       return;
@@ -100,48 +100,92 @@ app.get('/home/:id', function(req, res) {
 });
 
 app.listen(80, function () {
-    console.log('Node app is running on port 80');
+  console.log('Node app is running on port 80');
 });
 
 /* GET success page, respond by rendering success.ejs */
-app.get('/success', function(req, res) {
+app.get('/success', function (req, res) {
   res.render('success', { title: 'Success' });
 });
 
-app.get('/update-rating', function(req, res) {
-  var userId = req.query.id; ; // Get the user ID from the URL parameter
+app.get('/update-rating', function (req, res) {
+  var userId = req.query.id;; // Get the user ID from the URL parameter
   res.render('update-rating', { title: 'Update a Rating', userId: userId });
 });
 
 /* POST request to create user, redirect to success page if successful, show error message if unsuccessful */
-app.post('/update-rating', function(req, res) {
-  var userId = req.query.id; // Retrieve user ID from URL parameter
+app.post('/update-rating', function (req, res) {
   var media = req.body.media;
   var rating = req.body.rating;
   var type = req.body.type;
+  var change = req.body.change;
+  if (change == 'Update') {
+    if (type = 'Movie') {
 
-  if (type = 'Movie'){
-    var type = 'MovieRating';
-    var sql2 = `UPDATE MovieRating SET value = ${rating} WHERE id = ${userId} AND name = '${media}'`;
+      var sql2 = `UPDATE MovieRating SET value = ${rating} WHERE name = '${media}'`;
+
+
+    }
+    else {
+      var sql2 = `UPDATE ShowRating SET value = ${rating} WHERE name = '${media}'`;
+
+    }
   }
   else {
-    var type = 'ShowRating';
-    var sql2 = `UPDATE ShowRating SET value = ${rating} WHERE id = ${userId} AND  name = '${media}'`;
+
+    if (type = 'Movie') {
+
+      var sql2 = `DELETE FROM MovieRating WHERE name = '${media}'`;
+
+
+    }
+    else {
+      var sql2 = `DELETE FROM ShowRating WHERE name = '${media}'`;
+
+    }
+
   }
 
-  connection.query(sql2, function(err, result) {
-    if (err) {
-      res.send(err);
-      return;
-    }
-    console.log('Rating Updated');
-    res.redirect(`/rating-success?type=${type}&id=${userId}`);
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
   });
+
+  // Execute a SELECT query
+  var sql = `SELECT * FROM MovieRating WHERE name = '${media}'`;
+  connection.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    console.log("Before query")
+
+    console.log(result)
+
+
+  });
+
+  connection.query(sql2, (err, result) => {
+    if (err) throw err;
+
+    console.log("Update made")
+
+  });
+
+
+  var sql = `SELECT * FROM MovieRating WHERE name = '${media}'`;
+  connection.query(sql, (err, result, fields) => {
+    if (err) throw err;
+    console.log("After query")
+
+    console.log(result);
+
+    res.redirect('/rating-success');
+
+  });
+
 });
 
 
 // GET request for rating success page
-app.get('/rating-success', function(req, res) {
+app.get('/rating-success', function (req, res) {
   // Determine the appropriate table to query based on the 'type' parameter
   var tableName = req.query.type;
   var userID = req.query.id;
@@ -149,7 +193,7 @@ app.get('/rating-success', function(req, res) {
   // Perform a SELECT query to retrieve the updated data from the database
   var sql = `SELECT * FROM ${tableName} WHERE id = ${userID}`;
 
-  connection.query(sql, function(err, results) {
+  connection.query(sql, function (err, results) {
     if (err) {
       res.send(err);
       return;
@@ -161,7 +205,7 @@ app.get('/rating-success', function(req, res) {
 });
 
 //GET request for shows
-app.get('/shows', function(req, res) {
+app.get('/shows', function (req, res) {
   res.render('shows', { title: 'Shows' });
 });
 
@@ -186,50 +230,51 @@ app.get('/search', (req, res) => {
 
 //Generated Data for Advanced Queries -- REPLACE with sql outputs
 var dataQuery1 = [
-  ["Shows",'2021','23'],
-  ["Movies ",'2021','432'],
-  ["Shows",'2020','98'],
-  ['Movies','2020','435']
-]; 
+  ["Shows", '2021', '23'],
+  ["Movies ", '2021', '432'],
+  ["Shows", '2020', '98'],
+  ['Movies', '2020', '435']
+];
 
 var dataQuery2 = [
-  ["Divya",'Manirajan','23','11-02-1999'],
-  ["John ",'Smith','43','1-04-1949'],
-  ["boe",'jonnson','98','3-0-3333'],
-  ['hadf','adsf','435','2-2-2220']
-]; 
+  ["Divya", 'Manirajan', '23', '11-02-1999'],
+  ["John ", 'Smith', '43', '1-04-1949'],
+  ["boe", 'jonnson', '98', '3-0-3333'],
+  ['hadf', 'adsf', '435', '2-2-2220']
+];
 
 
 //GET request to set up advanced queries
-app.get('/AdvQueriesSubmit', function(req,res){
+app.get('/AdvQueriesSubmit', function (req, res) {
   res.render('AdvQueriesSubmit');
 });
 
 //POST request for Advanced Query 1
-app.post('/Query1', function(req, res) {
-  res.render('AdvQuery1Table', {sampleData:dataQuery1})}
-);  
+app.post('/Query1', function (req, res) {
+  res.render('AdvQuery1Table', { sampleData: dataQuery1 })
+}
+);
 
 //POST request for Advanced Query 2
-app.get('/Query2', function(req, res) {
+app.get('/Query2', function (req, res) {
 
   var platform = req.query.Platform;
 
-  var sql = "SELECT name, firstName, lastName, value FROM Users NATURAL JOIN MovieRating WHERE country IN (SELECT country FROM Platform WHERE platform = '"+platform+"')ORDER BY name LIMIT 15;";
+  var sql = "SELECT name, firstName, lastName, value FROM Users NATURAL JOIN MovieRating WHERE country IN (SELECT country FROM Platform WHERE platform = '" + platform + "')ORDER BY name LIMIT 15;";
 
-  connection.query(sql, function(err,dataQuery2){
+  connection.query(sql, function (err, dataQuery2) {
     if (err) throw err;
-    res.render('AdvQuery2Table', {sampleData:dataQuery2});
+    res.render('AdvQuery2Table', { sampleData: dataQuery2 });
   });
-}); 
+});
 
 //GET request for Top movies
-app.get('/topMovies', function(req, res){
-    var genre = req.query.genre;
-    var sYear = req.query.sYear;
-    var eYear = req.query.eYear;
+app.get('/topMovies', function (req, res) {
+  var genre = req.query.genre;
+  var sYear = req.query.sYear;
+  var eYear = req.query.eYear;
 
-    let sql = `SELECT name, m.releaseYear, value, genre 
+  let sql = `SELECT name, m.releaseYear, value, genre 
     FROM Movies as m Natural JOIN MovieRating as mr JOIN (
       SELECT MAX(value) as mxs, releaseYear
       FROM Movies Natural JOIN MovieRating
@@ -239,14 +284,14 @@ app.get('/topMovies', function(req, res){
     ORDER BY name
     LIMIT 15`
 
-    connection.query(sql, (err, result) => {
-      if (err) {
-        res.send(err);
-        return;
-      }
-      // res.render('shows', { title: 'Shows' });
-      // res.re(result)
-      res.send(result)
-    });
-
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    // res.render('shows', { title: 'Shows' });
+    // res.re(result)
+    res.send(result)
   });
+
+});
