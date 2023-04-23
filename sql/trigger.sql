46f37a64248e5fa6371ca1ccf0442cfa53e78232
rohanjs3 <rohanjs3@illinois.edu>
2023-04-23 15:44:48 -0500

    trigger sql

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,1 +1,5 @@
-# Add code here
+CREATE TRIGGER boundRating BEFORE UPDATE ON MovieRating FOR EACH ROW BEGIN          
+    IF new.value > 10 THEN              
+        SET NEW.value = 10;          
+    END IF;      
+END;
