d299d2fe219ba31e6cacbc307eb9cc2577c076cd
Anthony Doan <=>
2023-04-25 22:38:17 -0500

    Rename trigger names to make it both unique

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,18 +1,28 @@
 /*
-    boundRating triggers that allow for Update action using condition (IF Statement) 
+    boundMovieRating and boundShowRating
+    Triggers that allow for Update action using condition (IF Statement) 
     on MovieRating and ShowRating.
 
-    Sets bounds on if users set a value greater than 10 to cap it at 10.
+    Sets bounds on:
+        - if users set a value greater than 10 to set it to 10
+        - if users set a value less than 1 to set it to 1
     This is done before any UPDATES.
 */
-CREATE TRIGGER boundRating BEFORE UPDATE ON MovieRating FOR EACH ROW BEGIN          
-    IF new.value > 10 THEN           
+
+CREATE TRIGGER boundMovieRating BEFORE UPDATE ON MovieRating FOR EACH ROW 
+BEGIN          
+    IF NEW.value > 10 THEN           
         SET NEW.value = 10;          
+    ELSEIF NEW.value < 1 THEN           
+        SET NEW.value = 1;          
     END IF;      
 END;
 
-CREATE TRIGGER boundRating BEFORE UPDATE ON ShowRating FOR EACH ROW BEGIN          
-    IF new.value > 10 THEN              
+
+CREATE TRIGGER boundShowRating BEFORE UPDATE ON ShowRating FOR EACH ROW BEGIN          
+    IF NEW.value > 10 THEN           
         SET NEW.value = 10;          
-    END IF;      
+    ELSEIF NEW.value < 1 THEN           
+        SET NEW.value = 1;          
+    END IF;         
 END;
