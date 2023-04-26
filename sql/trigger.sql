1600f8b12be04413945f2edf8a415c433a197a5a
Anthony Doan <=>
2023-04-25 22:30:19 -0500

    Update procedure and rename file. Also add comments to describe SQL files

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,5 +1,12 @@
+/*
+    boundRating triggers that allow for Update action using condition (IF Statement) 
+    on MovieRating and ShowRating.
+
+    Sets bounds on if users set a value greater than 10 to cap it at 10.
+    This is done before any UPDATES.
+*/
 CREATE TRIGGER boundRating BEFORE UPDATE ON MovieRating FOR EACH ROW BEGIN          
-    IF new.value > 10 THEN              
+    IF new.value > 10 THEN           
         SET NEW.value = 10;          
     END IF;      
 END;
